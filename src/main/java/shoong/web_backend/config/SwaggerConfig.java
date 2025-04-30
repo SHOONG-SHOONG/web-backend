a8987b7a943f68e8b66bdf38f345623e0ba27e4e
wns5120 <ghwns7171@naver.com>
2025-04-30 12:15:35 +0900

    fix: SwaggerConfig 추가

--- src/main/java/shoong/web_backend/config/SwaggerConfig.java
+++ src/main/java/shoong/web_backend/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
-package shoong.web_backend.Config;
+package shoong.web_backend.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
