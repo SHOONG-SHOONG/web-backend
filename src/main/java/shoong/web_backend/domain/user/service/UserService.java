df91176e12ffaa54f57baf95f39587f86e2884e5
eundeom <36tfuu@gmail.com>
2025-04-30 09:35:40 +0900

    feat : user DTO 업데이트

--- src/main/java/shoong/web_backend/domain/user/service/UserService.java
+++ src/main/java/shoong/web_backend/domain/user/service/UserService.java
@@ -1,11 +1,11 @@
 package shoong.web_backend.domain.user.service;
 
 import org.springframework.stereotype.Service;
-import shoong.web_backend.domain.user.dto.UserDTO;
+import shoong.web_backend.domain.user.dto.UserLoginResponseDTO;
 
 @Service
 public interface UserService {
     boolean existsByKakaoId(Long kakaoId);
-    UserDTO findByKakaoId(Long kakaoId);
-    UserDTO save(UserDTO user);
+    UserLoginResponseDTO findByKakaoId(Long kakaoId);
+    UserLoginResponseDTO save(UserLoginResponseDTO user);
 }
