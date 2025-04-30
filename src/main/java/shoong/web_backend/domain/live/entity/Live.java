834983261d001309483a9bc156f1ad948b70bfb6
wns5120 <ghwns7171@naver.com>
2025-04-30 10:26:32 +0900

    fix: Live 엔티티 liveDate 수정

--- src/main/java/shoong/web_backend/domain/live/entity/Live.java
+++ src/main/java/shoong/web_backend/domain/live/entity/Live.java
@@ -9,6 +9,7 @@ import shoong.web_backend.domain.live.enums.LiveStatus;
 import shoong.web_backend.domain.live_item.entity.LiveItem;
 import shoong.web_backend.domain.user.entity.User;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +30,7 @@ public class Live {
 
     private String imageUrl;
 
-    private LocalDateTime liveDate;
+    private LocalDate liveDate;
 
     @Enumerated(EnumType.STRING)
     private LiveStatus liveStatus;
