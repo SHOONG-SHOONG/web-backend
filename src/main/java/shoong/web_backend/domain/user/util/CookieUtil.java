7ad77e98a893b9bcc9b907970ae51c6513b43221
woody6624 <woody6624@gmail.com>
2025-05-23 14:30:29 +0900

    fix: 토큰 https 부여

--- src/main/java/shoong/web_backend/domain/user/util/CookieUtil.java
+++ src/main/java/shoong/web_backend/domain/user/util/CookieUtil.java
@@ -5,8 +5,8 @@ import jakarta.servlet.http.Cookie;
 public class CookieUtil {
     public static Cookie createCookie(String key, String value, Integer expiredS) {
         Cookie cookie = new Cookie(key, value);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(false);
+        // cookie.setHttpOnly(true);
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(expiredS);
         return cookie;
