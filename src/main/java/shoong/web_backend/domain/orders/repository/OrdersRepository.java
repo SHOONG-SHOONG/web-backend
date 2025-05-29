a8abd282b5441d89f384a74287d02e12b04e9d9c
DoomchitYJ <poj1004@inha.edu>
2025-05-29 10:42:41 +0900

    fix: 주문 완료 상태와 주문중 상태의 리스트 호출 API 분리

--- src/main/java/shoong/web_backend/domain/orders/repository/OrdersRepository.java
+++ src/main/java/shoong/web_backend/domain/orders/repository/OrdersRepository.java
@@ -2,10 +2,12 @@ package shoong.web_backend.domain.orders.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import shoong.web_backend.domain.orders.entity.Orders;
+import shoong.web_backend.domain.orders.enums.OrderStatus;
 import shoong.web_backend.domain.wishlist.entity.Wishlist;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
-    List<Orders> findByUserIdOrderByOrderDateDesc(Long userId);
+    Optional<Orders> findByUserIdAndOrderStatus(Long userId, OrderStatus orderStatus);
 }
