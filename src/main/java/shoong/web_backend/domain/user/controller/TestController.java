976c4c0e9084e355592b9b3e6c36c5100f15d2e4
HoJun <73707598+wns5120@users.noreply.github.com>
2025-06-04 11:34:34 +0900

    fix : NPE 에러 수정

--- src/main/java/shoong/web_backend/domain/user/controller/TestController.java
+++ src/main/java/shoong/web_backend/domain/user/controller/TestController.java
@@ -38,7 +38,8 @@ public class TestController {
                 .bdate(user.getBirthDay())
                 .registrationNumber(user.getRegistrationNumber() != null ? user.getRegistrationNumber() : null)
                 .userStatus(user.getUserStatus().name())
-                .brandId(user.getBrand().getBrandId() != null ? user.getBrand().getBrandId() : null)
+                .brandId(user.getBrand() != null ? user.getBrand().getBrandId() : null)
+//                .brandId(user.getBrand().getBrandId() != null ? user.getBrand().getBrandId() : null)
                 .build();
 
         return ResponseEntity.ok(userDTO);
